497b21d7f6119422ca66e7035c0d02e8ec245fe0
Vivek Parmar <vp393001@gmail.com>
2020-05-12 07:01:46 +0000

    Modified run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,24 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=vp393001/mlmicroservice
+
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run ml-app-pod\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=ml-app-pod
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
+
 
 # Step 4:
 # Forward the container port to a host
+kubectl port-forward ml-app-pod 8000:80
+
 
