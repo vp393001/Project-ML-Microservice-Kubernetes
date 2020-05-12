eceeef9e4db1f66e89e970bd7842a0551af0ee76
Vivek Parmar <vp393001@gmail.com>
2020-05-12 06:33:31 +0000

    Changed tag

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=mlmicrocroservice .
+docker build --tag=mlmicroservice .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -d -p 8000:80 mlmicrocroservice
+docker run -p 8000:80 mlmicroservice
