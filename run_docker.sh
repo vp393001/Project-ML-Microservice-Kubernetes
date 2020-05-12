b7b4ff5490fb6950b2ac23ae022090b021f45079
Vivek Parmar <vp393001@gmail.com>
2020-05-12 06:01:45 +0000

    Changed port

--- run_docker.sh
+++ run_docker.sh
@@ -4,7 +4,7 @@
 
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=MLMicroService .
+docker build --tag=mlmicrocroservice .
 
 # Step 2: 
 # List docker images
@@ -12,4 +12,4 @@ docker image ls
 
 # Step 3: 
 # Run flask app
-docker run -d -p 8080:8080 MLMicroService
+docker run -d -p 8000:80 mlmicrocroservice
